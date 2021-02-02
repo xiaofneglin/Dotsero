77f5bae6dec031bc56dc6da592df788ca3b67e7c
XiaoFeng. Lin <xiaofenglin121@gmail.com>
2021-02-02 21:25:44 +0800

    Cleaned up ActorOf().

---
+++ DotseroDemo/Message/AbstractMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotseroDemo.Message
+{
+    public abstract class AbstractMessage
+    {
+        public virtual void Do() { }
+    }
+}
---
+++ DotseroDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfast
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
